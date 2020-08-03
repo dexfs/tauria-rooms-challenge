c95e5d90f67c829a79856d18bbebb05dbd825f19
André Santos <andre@dexdev.com.br>
2020-08-03 08:58:31 -0300

    refactor: uuid validation

--- src/modules/rooms/validations/changeRoomHostValidation.ts
+++ src/modules/rooms/validations/changeRoomHostValidation.ts
@@ -2,7 +2,7 @@ import { Joi } from 'express-validation';
 
 export default {
   body: Joi.object({
-    roomId: Joi.string().required(),
-    newHost: Joi.string().required(),
+    roomId: Joi.string().required().uuid(),
+    newHost: Joi.string().required().uuid(),
   }),
 };
