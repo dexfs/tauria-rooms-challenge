9bde0e25ff4532669a92bc5e243ed0bd2bca440c
André Santos <andre@dexdev.com.br>
2020-08-03 08:59:39 -0300

    fix: repository filename

--- src/modules/rooms/repositories/RoomParticipantRepository.ts
+++ src/modules/rooms/repositories/RoomParticipantRepository.ts
@@ -2,6 +2,6 @@ import { EntityRepository, Repository } from 'typeorm';
 import RoomParticipant from '../entities/RoomParticipant';
 
 @EntityRepository(RoomParticipant)
-class RoomRepository extends Repository<RoomParticipant> {}
+class RoomParticipantRepository extends Repository<RoomParticipant> {}
 
-export default RoomRepository;
+export default RoomParticipantRepository;
