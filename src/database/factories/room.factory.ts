f9d67c707ec92195d92a1d0aeb673190f84c266d
André Santos <andre@dexdev.com.br>
2020-08-03 01:41:49 -0300

    chore: factory improvement

--- src/database/factories/room.factory.ts
+++ src/database/factories/room.factory.ts
@@ -5,6 +5,7 @@ import User from '@modules/users/entities/User';
 
 define(Room, (faker: typeof Faker) => {
   const room = new Room();
+  room.id = faker.random.uuid();
   room.name = faker.name.title();
   room.hostUser = factory(User)() as any;
   room.capacityLimit = faker.random.number({ min: 5, max: 100 });
